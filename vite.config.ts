75b62ac4cd2f90b034f609d1d9f091cb4c00b893
RuRu26 <ruhi267chetia@gmail.com>
2025-06-14 17:24:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/[YOUR_REPO_NAME]/' : '/',
+  base: mode === 'production' ? '/posh-photo-grid/' : '/',
   server: {
     host: "::",
     port: 8080,
